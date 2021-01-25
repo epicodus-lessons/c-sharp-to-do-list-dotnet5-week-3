d1c3c26486ecbb9216e1e517fef1ed91da8d9ddf
Travis Scott <travisty12@gmail.com>
2021-01-24 18:05:53 -0800

    update model and controller for ef core

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -1,133 +1,133 @@
-using MySql.Data.MySqlClient;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
-using ToDoList.Models;
-using System;
+// using MySql.Data.MySqlClient;
+// using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using System.Collections.Generic;
+// using ToDoList.Models;
+// using System;
 
-namespace ToDoList.Tests
-{
+// namespace ToDoList.Tests
+// {
 
-  [TestClass]
-  public class ItemTests : IDisposable
-  {
+//   [TestClass]
+//   public class ItemTests : IDisposable
+//   {
 
-    public void Dispose()
-    {
-      Item.ClearAll();
-    }
-    public void ItemTest()
-    {
-      DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list_test;";
-    }
+//     public void Dispose()
+//     {
+//       Item.ClearAll();
+//     }
+//     public void ItemTest()
+//     {
+//       DBConfiguration.ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list_test;";
+//     }
 
-    [TestMethod]
-    public void ItemConstructor_CreatesInstanceOfItem_Item()
-    {
-      Item newItem = new Item("test");
-      Assert.AreEqual(typeof(Item), newItem.GetType());
-    }
+//     [TestMethod]
+//     public void ItemConstructor_CreatesInstanceOfItem_Item()
+//     {
+//       Item newItem = new Item("test");
+//       Assert.AreEqual(typeof(Item), newItem.GetType());
+//     }
 
-    [TestMethod]
-    public void GetDescription_ReturnsDescription_String()
-    {
-      //Arrange
-      string description = "Walk the dog.";
+//     [TestMethod]
+//     public void GetDescription_ReturnsDescription_String()
+//     {
+//       //Arrange
+//       string description = "Walk the dog.";
 
-      //Act
-      Item newItem = new Item(description);
-      string result = newItem.Description;
+//       //Act
+//       Item newItem = new Item(description);
+//       string result = newItem.Description;
 
-      //Assert
-      Assert.AreEqual(description, result);
-    }
+//       //Assert
+//       Assert.AreEqual(description, result);
+//     }
 
-    [TestMethod]
-    public void SetDescription_SetDescription_String()
-    {
-      //Arrange
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
+//     [TestMethod]
+//     public void SetDescription_SetDescription_String()
+//     {
+//       //Arrange
+//       string description = "Walk the dog.";
+//       Item newItem = new Item(description);
 
-      //Act
-      string updatedDescription = "Do the dishes";
-      newItem.Description = updatedDescription;
-      string result = newItem.Description;
+//       //Act
+//       string updatedDescription = "Do the dishes";
+//       newItem.Description = updatedDescription;
+//       string result = newItem.Description;
 
-      //Assert
-      Assert.AreEqual(updatedDescription, result);
-    }
+//       //Assert
+//       Assert.AreEqual(updatedDescription, result);
+//     }
 
-    [TestMethod]
-    public void GetAll_ReturnsEmptyList_ItemList()
-    {
-      // Arrange
-      List<Item> newList = new List<Item> { };
+//     [TestMethod]
+//     public void GetAll_ReturnsEmptyList_ItemList()
+//     {
+//       // Arrange
+//       List<Item> newList = new List<Item> { };
 
-      // Act
-      List<Item> result = Item.GetAll();
+//       // Act
+//       List<Item> result = Item.GetAll();
 
-      // Assert
-      CollectionAssert.AreEqual(newList, result);
-    }
+//       // Assert
+//       CollectionAssert.AreEqual(newList, result);
+//     }
 
-    [TestMethod]
-    public void GetAll_ReturnsItems_ItemList()
-    {
-      //Arrange
-      string description01 = "Walk the dog";
-      string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      newItem1.Save();
-      Item newItem2 = new Item(description02);
-      newItem2.Save();
-      List<Item> newList = new List<Item> { newItem1, newItem2 };
+//     [TestMethod]
+//     public void GetAll_ReturnsItems_ItemList()
+//     {
+//       //Arrange
+//       string description01 = "Walk the dog";
+//       string description02 = "Wash the dishes";
+//       Item newItem1 = new Item(description01);
+//       newItem1.Save();
+//       Item newItem2 = new Item(description02);
+//       newItem2.Save();
+//       List<Item> newList = new List<Item> { newItem1, newItem2 };
 
-      //Act
-      List<Item> result = Item.GetAll();
+//       //Act
+//       List<Item> result = Item.GetAll();
 
-      //Assert
-      CollectionAssert.AreEqual(newList, result);
-    }
+//       //Assert
+//       CollectionAssert.AreEqual(newList, result);
+//     }
 
-    [TestMethod]
-    public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
-    {
-      // Arrange, Act
-      Item firstItem = new Item("Mow the lawn");
-      Item secondItem = new Item("Mow the lawn");
+//     [TestMethod]
+//     public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
+//     {
+//       // Arrange, Act
+//       Item firstItem = new Item("Mow the lawn");
+//       Item secondItem = new Item("Mow the lawn");
 
-      // Assert
-      Assert.AreEqual(firstItem, secondItem);
-    }
+//       // Assert
+//       Assert.AreEqual(firstItem, secondItem);
+//     }
 
-    [TestMethod]
-    public void Save_SavesToDatabase_ItemList()
-    {
-      //Arrange
-      Item testItem = new Item("Mow the lawn");
+//     [TestMethod]
+//     public void Save_SavesToDatabase_ItemList()
+//     {
+//       //Arrange
+//       Item testItem = new Item("Mow the lawn");
 
-      //Act
-      testItem.Save();
-      List<Item> result = Item.GetAll();
-      List<Item> testList = new List<Item>{testItem};
+//       //Act
+//       testItem.Save();
+//       List<Item> result = Item.GetAll();
+//       List<Item> testList = new List<Item>{testItem};
 
-      //Assert
-      CollectionAssert.AreEqual(testList, result);
-    }
+//       //Assert
+//       CollectionAssert.AreEqual(testList, result);
+//     }
 
-    [TestMethod]
-    public void Find_ReturnsCorrectItemFromDatabase_Item()
-    {
-      //Arrange
-      Item newItem = new Item("Mow the lawn");
-      newItem.Save();
-      Item newItem2 = new Item("Wash dishes");
-      newItem2.Save();
+//     [TestMethod]
+//     public void Find_ReturnsCorrectItemFromDatabase_Item()
+//     {
+//       //Arrange
+//       Item newItem = new Item("Mow the lawn");
+//       newItem.Save();
+//       Item newItem2 = new Item("Wash dishes");
+//       newItem2.Save();
 
-      //Act
-      Item foundItem = Item.Find(newItem.Id);
-      //Assert
-      Assert.AreEqual(newItem, foundItem);
-    }
-  }
-}
+//       //Act
+//       Item foundItem = Item.Find(newItem.Id);
+//       //Assert
+//       Assert.AreEqual(newItem, foundItem);
+//     }
+//   }
+// }
--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,34 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
+    private readonly ToDoListContext _db;
 
-    [HttpGet("/categories/{categoryId}/items/new")]
-    public ActionResult New(int categoryId)
+    public ItemsController(ToDoListContext db)
     {
-      Category category = Category.Find(categoryId);
-      return View(category);
+      _db = db;
     }
 
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
+    public ActionResult Index()
     {
-      Item.ClearAll();
-      return View();
-    }
-
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Show(int categoryId, int itemId)
-    {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
+      List<Item> model = _db.Items.ToList();
       return View(model);
     }
   }
